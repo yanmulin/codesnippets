d2803ab8ff7a6fa949fec3650dbc325c5e7e066e
yanmulin <lyuweny@gmail.com>
2024-04-24 18:04:03 -0700

    add a reflection api example

---
+++ java-examples/src/main/java/io/yanmulin/codesnippets/basic/ReflectionExamples.java
@@ -0,0 +1,24 @@
+package io.yanmulin.codesnippets.basic;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ReflectionExamples {
+
+    public void hello(String name) {
+        System.out.println("hello, " + name);
+    }
+
+    public void method() {
+        try {
+            Method method = getClass().getMethod("hello", String.class);
+            method.invoke(this, "John");
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReflectionExamples().method();
+    }
+}
