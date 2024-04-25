9ce19b07e0244966b65643e74f5f2875229b928d
yanmulin <lyuweny@gmail.com>
2024-04-24 18:03:19 -0700

    add spring examples

---
+++ spring-examples/src/main/java/io/yanmulin/codesnippets/spring/aspect/MyAspect.java
@@ -0,0 +1,27 @@
+package io.yanmulin.codesnippets.spring.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class MyAspect {
+
+    @Before("execution(void *.run())")
+    public void before(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        log.info("before invoking {}", signature.getMethod().getName());
+    }
+
+    @After("execution(void *.run())")
+    public void after(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        log.info("before invoking {}", signature.getMethod().getName());
+    }
+}
---
+++ spring-examples/src/main/java/io/yanmulin/codesnippets/spring/config/AopConfiguration.java
@@ -0,0 +1,9 @@
+package io.yanmulin.codesnippets.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy
+@Configuration
+public class AopConfiguration {
+}
