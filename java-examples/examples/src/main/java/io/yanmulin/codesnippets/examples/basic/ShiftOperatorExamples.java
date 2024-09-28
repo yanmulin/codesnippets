2da863d6f7129379bc3ff809877aadf98f8475d0
yanmulin <lyuweny@gmail.com>
2024-09-28 10:55:29 -0700

    update shift op example

--- java-examples/examples/src/main/java/io/yanmulin/codesnippets/examples/basic/ShiftOperatorExamples.java
+++ java-examples/examples/src/main/java/io/yanmulin/codesnippets/examples/basic/ShiftOperatorExamples.java
@@ -5,8 +5,12 @@ public class ShiftOperatorExamples {
         System.out.println("127 << 1 = " + (127 << 1));
         System.out.println("127 >> 1 = " + (127 >> 1));
         System.out.println("127 >>> 1 = " + (127 >>> 1));
-        System.out.println("-127 >> 1 = " + (-127 >> 1));       // signed shift, expect -64
-        System.out.println("-127 >>> 1 = " + (-127 >>> 1));     // unsigned shift, expect 2147483584
+
+        System.out.println(String.format("-127 = 0b%s", Integer.toBinaryString(-127)));
+        // signed shift, expect -64
+        System.out.println(String.format("-127 >> 1 = %d(0b%s)", -127 >> 1, Integer.toBinaryString(-127 >> 1)));
+        // unsigned shift, expect 2147483584
+        System.out.println(String.format("-127 >>> 1 = %d(0b%s)", -127 >>> 1, Integer.toBinaryString(-127 >>> 1)));
     }
 
     public static void main(String[] args) {
