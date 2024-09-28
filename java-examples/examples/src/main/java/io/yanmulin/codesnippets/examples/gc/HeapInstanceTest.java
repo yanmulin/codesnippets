1001ca8a21183c59aabc0395eb15f4a9d6e19545
yanmulin <lyuweny@gmail.com>
2024-09-28 10:55:46 -0700

    Add heap example

--- java-examples/examples/src/main/java/io/yanmulin/codesnippets/examples/gc/HeapInstanceTest.java
+++ java-examples/examples/src/main/java/io/yanmulin/codesnippets/examples/gc/HeapInstanceTest.java
@@ -1,2 +1,26 @@
-package io.yanmulin.codesnippets.examples.gc;public class HeapInstanceTest {
+package io.yanmulin.codesnippets.examples.gc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class HeapInstanceTest {
+    byte[] buffer = new byte[new Random().nextInt(4096 * 4096)];
+
+    public static void main(String[] args) {
+        System.out.println("process id " + ProcessHandle.current().pid());
+        System.out.println("initial memory(mb) " + Runtime.getRuntime().totalMemory() / 1024 / 1024);
+        System.out.println("max memory(mb) " + Runtime.getRuntime().maxMemory() / 1024 / 1024);
+        List<HeapInstanceTest> list = new ArrayList<>();
+        while (true) {
+            list.add(new HeapInstanceTest());
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException exception) {
+                exception.printStackTrace();
+            }
+        }
+
+
+    }
 }
