28e6f399436f48ad1b37a7cc983bad044e2f6975
yanmulin <lyuweny@gmail.com>
2024-11-03 09:39:21 -0800

    add thread pool examples

--- java-examples/examples/src/main/java/io/yanmulin/codesnippets/examples/concurrency/ThreadPoolExecutorExamples.java
+++ java-examples/examples/src/main/java/io/yanmulin/codesnippets/examples/concurrency/ThreadPoolExecutorExamples.java
@@ -2,9 +2,9 @@ package io.yanmulin.codesnippets.examples.concurrency;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @Slf4j
 public class ThreadPoolExecutorExamples {
@@ -28,7 +28,7 @@ public class ThreadPoolExecutorExamples {
 
     private void testInitParams() {
         ThreadPoolExecutor executor = new ThreadPoolExecutor(
-                4, 16, 2, TimeUnit.SECONDS,
+                4, 16, 2, SECONDS,
                 new LinkedBlockingQueue<>(4), new ThreadPoolExecutor.CallerRunsPolicy()
         );
 
@@ -71,10 +71,73 @@ public class ThreadPoolExecutorExamples {
         executor.shutdown();
     }
 
-    public static void main(String[] args) {
-        new ThreadPoolExecutorExamples().testInitParams();
+    class SleepTask implements Callable<Void> {
+
+        private final int id;
+        private final long sleepSeconds;
+
+        private final CountDownLatch runningLatch = new CountDownLatch(1);
+
+        SleepTask(int id, long sleepSeconds) {
+            this.id = id;
+            this.sleepSeconds = sleepSeconds;
+        }
+
+        @Override
+        public Void call() throws InterruptedException {
+            try {
+                System.out.println("task " + id + " starts");
+                runningLatch.countDown();
+                SECONDS.sleep(sleepSeconds);
+            } catch (InterruptedException e) {
+                System.out.println("task " + id + " exception " + e);
+                throw e;
+            } finally {
+                System.out.println("task " + id + " ended");
+            }
+            return null;
+        }
     }
 
+    class TrackingThread extends Thread {
+        public TrackingThread(Runnable r) { super(r); }
+        @Override
+        public void run() {
+            try {
+                System.out.println("thread " + this + " started");
+                super.run();
+            } finally {
+                System.out.println("thread " + this + " ended");
+            }
+        }
+    }
 
+    class TrackingThreadFactory implements ThreadFactory {
+        @Override
+        public Thread newThread(Runnable r) {
+            return new TrackingThread(r);
+        }
+    }
 
+    public void cancelTask() throws InterruptedException {
+        ExecutorService executor = new ThreadPoolExecutor(2, 2,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(), new TrackingThreadFactory());
+
+        SleepTask t1 = new SleepTask(1, 5);
+        Future<Void> f1 = executor.submit(t1);
+        executor.submit(new SleepTask(2, 30));
+        Future<Void> f3 = executor.submit(new SleepTask(3, 100));
+        executor.submit(new SleepTask(4, 30));
+        t1.runningLatch.await();
+        f1.cancel(true); // cancel running
+        f3.cancel(true); // cancel before start
+        executor.shutdown();
+        executor.awaitTermination(100, SECONDS);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+//        new ThreadPoolExecutorExamples().testInitParams();
+        new ThreadPoolExecutorExamples().cancelTask();
+    }
 }
