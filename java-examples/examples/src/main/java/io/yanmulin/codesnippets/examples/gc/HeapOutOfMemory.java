7ef58925b0125bee7ecbe4c5185721d0cd004135
yanmulin <lyuweny@gmail.com>
2024-11-03 09:39:44 -0800

    add OOM example

--- java-examples/examples/src/main/java/io/yanmulin/codesnippets/examples/gc/HeapOutOfMemory.java
+++ java-examples/examples/src/main/java/io/yanmulin/codesnippets/examples/gc/HeapOutOfMemory.java
@@ -1,2 +1,24 @@
-package io.yanmulin.codesnippets.examples.gc;public class HeapOutOfMemory {
+package io.yanmulin.codesnippets.examples.gc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeapOutOfMemory {
+    private class People {
+        String name;
+        Integer age;
+        String job;
+        String sex;
+    }
+
+    public void addObjects() {
+        List<People> people = new ArrayList<>();
+        while (true) {
+            people.add(new People());
+        }
+    }
+
+    public static void main(String[] args) {
+        new HeapOutOfMemory().addObjects();
+    }
 }
