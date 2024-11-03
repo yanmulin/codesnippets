5d95a2050651e8bc9aa68c98973433e60bbed6b2
yanmulin <lyuweny@gmail.com>
2024-11-03 09:40:11 -0800

    add jvm perf module

---
+++ spring-examples/jvm-performance-examples/src/main/java/io/yanmulin/codesnippets/performance/JVMPerformanceApplication.java
@@ -0,0 +1,11 @@
+package io.yanmulin.codesnippets.performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JVMPerformanceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JVMPerformanceApplication.class, args);
+    }
+}
